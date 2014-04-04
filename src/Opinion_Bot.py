8b2ff44602d1e62a142b679cd7028350449a8d00
Daniel Scarafoni <dscarafo@u.rochester.edu>
2014-04-04 15:28:19 -0400

    starting command line argument parsing

--- src/Opinion_Bot.py
+++ src/Opinion_Bot.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python
 import sys
-
-Class IO:
+import argparse
+#args gramsize from_file -pt print_text -pcsv printcsv
+#gramsize-int
+#from_file- file we're pulling from, must end in .txt or .csv
+#-pt print_text- prints output in text, to file if specified (default option if no arg is specified)
+#-ptcsv- prints mtable to csv, to stdio if not specified
+parser = argparse.ArgumentParser()
+parser.add_argument('-i','--infile'
+parser.parse_args()
