06f499bdfd770562884ed582f43349041fc67e53
J. Hassler Thurston <jthurst3@u.rochester.edu>
2014-04-05 17:16:01 -0400

    counts

--- src/ngrams.py
+++ src/ngrams.py
@@ -3,8 +3,16 @@
 # modified from http://stackoverflow.com/questions/17531684/n-grams-in-python-four-five-six-grams
 
 from nltk.util import ngrams
-sentence = 'this is a foo bar sentences and i want to ngramize it'
-n = 6
-sixgrams = ngrams(sentence.split(), n)
-for grams in sixgrams:
-  print grams
+from collections import Counter
+sentence = 'this is a foo bar foo bar i want to ngramize it'
+
+MAX_NGRAM = 6
+counts = []
+dicts = []
+
+for n in range(1,MAX_NGRAM+1):
+    n_grams = ngrams(sentence.split(), n)
+    counts.append(n_grams)
+    dicts.append(Counter(n_grams))
+
+print dicts[1]
