159a80f2a1e4fa226a1fd541c6262746982f7515
scarafoni <dscarafo@u.rochester.edu>
2014-04-05 14:11:17 -0400

    first command line argument

--- src/Opinion_Bot.py
+++ src/Opinion_Bot.py
@@ -6,6 +6,10 @@ import argparse
 #from_file- file we're pulling from, must end in .txt or .csv
 #-pt print_text- prints output in text, to file if specified (default option if no arg is specified)
 #-ptcsv- prints mtable to csv, to stdio if not specified
-parser = argparse.ArgumentParser()
-parser.add_argument('-i','--infile'
-parser.parse_args()
+init_args = argparse.ArgumentParser()
+init_args.add_argument('input', help='the input file to be read, must be either .csv (probability table) or .txt (plain text)',
+                       type=argparse.FileType('r')) 
+init_args.add_argument('output', help='specify the output of the file, must be either .txt (plain text) or .csv (markov table), if non specified, will print plain text to stdio',
+                       type=argparse.FileType('r'),
+                       default=sys.stdout)
+init_args.parse_args()
