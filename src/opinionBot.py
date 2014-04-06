e24a72355d7a9beb63c62ffeea66ef820a294042
J. Hassler Thurston <jthurst3@u.rochester.edu>
2014-04-06 00:28:01 -0400

    limbaugh archives to mongo

--- src/opinionBot.py
+++ src/opinionBot.py
@@ -4,16 +4,31 @@
 # HackNY Fall 2014 Hackathon
 # April 5-6, 2014
 
+from getLimbaughSpeeches import *
 from toMongo import *
+import os
+
+# gets all Rush Limbaugh transcripts
+# from http://stackoverflow.com/questions/3964681/find-all-files-in-directory-with-extension-txt-with-python
+def get_limbaugh():
+    files = []
+    for file in os.listdir("../personalities/limbaugh"):
+        if file.endswith(".txt"):
+            files.append(file)
+    return files
 
 
-def main():
-    f = open('../personalities/mac_lover.txt')
-    tm = toMongo("Rush Limbaugh", f.read())
-    print tm
-    f.close()
+def get_speeches():
+    get_limbaugh_speeches('../personalities/limbaugh/',10)
+
+def write_to_mongo():
+    l = get_limbaugh()
+    for file in l:
+        f = open('../personalities/limbaugh/'+file, 'r').read().split('\n')
+        if f[1] != '':
+            toMongo(f[0], ''.join(f[1:]))
 
 if __name__ == '__main__':
-    main()
+    write_to_mongo()
 
 
