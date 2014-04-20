300cd1c07144b7b1f2426339a04c96e470e7fe6c
scarafoni <dscarafo@u.rochester.edu>
2014-04-20 01:59:52 -0400

    might be superfluous

--- NLTK_Bot/NLTK_Bot.py
+++ NLTK_Bot/NLTK_Bot.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python
 import sys
 import nltk
+import string
+
 
 class Listener:
     '''reads in input from a file and builds a markov table as a result'''
     n = 0
-    nltk.ngram model
-    
-    def __init__(self,n,text):
+
+    def __init__(self, n, text):
         self.n = n
-        #strip the punctuation and split the string
-        print('text before',text)
-        text = text.translate(None,string.punctuation)
+        # strip the punctuation and split the string
+        print('text before', text)
+        text = text.translate(None, string.punctuation)
         print(text)
-
-
-
-if __name__ == '__main__':
-    model = Listener(2,"this sentence has a . misplaced period,")
-
