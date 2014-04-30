ab7412f92b15eedb4cc5b3b740cee299254b9a71
scarafoni <dscarafo@u.rochester.edu>
2014-04-30 12:55:45 -0400

    listener uses transition table

--- Custom_Bot/custom_bot.py
+++ Custom_Bot/custom_bot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import sys
-import nltk
+from nltk import ngrams
 import string
 
 
@@ -9,31 +9,34 @@ class Listener():
     n = 0
 
     def __init__(self, n, text):
-        self.n = n
         # strip the punctuation and split the string
         text = text.translate(None, string.punctuation)
         text = ' '.join(text.split())
         # split on whitespace
         self.text = text.split()
-        self.model = nltk.NgramModel(self.n, self.text)
+        # enter the relevant information
+        self.n = n
+        self.grams = ngrams(self.text, n)
+        self.table = Transition_Table(self.grams)
 
     def make_story(self, size):
         starters = self.model.generate(10)[-2:]
         word_list = self.model.generate(size, starters)
         print(' '.join(word_list))
 
+
 class Transition_Table:
     '''holds n-gram transitions for a markov chain'''
 
-    def __init__(self,grams):
-        self.table = dict.fromkeys(grams,dict.fromkeys(grams,0))
+    def __init__(self, grams):
+        self.table = dict.fromkeys(grams, dict.fromkeys(grams, 0))
         for gram1 in grams:
             for gram2 in grams:
-               print(self.table[gram1][gram2])
-
+                print('')
+                # print(gram1, gram2, self.table[gram1][gram2])
 
 
 if __name__ == '__main__':
-    story = open('../stories/'+sys.argv[1]+'.txt', 'r').read()
+    story = open('../texts/'+sys.argv[1], 'r').read()
+    print(story)
     listener = Listener(int(sys.argv[2]), story)
-    listener.make_story(100)
