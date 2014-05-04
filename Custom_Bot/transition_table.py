1ca6680f25d4c9c41e5764c66a87178df176099d
scarafoni <dscarafo@u.rochester.edu>
2014-05-04 15:40:44 -0400

    normalize and get row seem to work

--- Custom_Bot/transition_table.py
+++ Custom_Bot/transition_table.py
@@ -11,40 +11,30 @@ class Transition_Table:
         for i in range(len(text_list)-n):
             prev_gram = tuple(text_list[i:i+n])
             next_gram = tuple(text_list[i+1:i+n+1])
-            print(prev_gram, next_gram)
-            if next_gram in self.table[prev_gram]:
+            # print(prev_gram, next_gram)
+            if (prev_gram, next_gram) in self.table:
                 self.table[prev_gram, next_gram] += 1
             else:
                 self.table[prev_gram, next_gram] = 1
-            print('row after', self.table)
-            print()
         self.normalize_table()
-
-    '''
-    def gen_dic(self, grams):
-        for gram1 in grams:
-            # for gram2 in grams:
-                # self.table[gram1, gram2] = 0
-    '''
 
     # normalize the table
     def normalize_table(self):
         for gram in self.grams:
             row = self.get_row(gram)
             tot = float(sum(row[col] for col in row))
-            print(gram)
-            print(row)
-            print(len(row))
-            for col_gram in row:
+            for key, val in row.iteritems():
                 if tot != 0:
-                    row[col_gram] /= tot
-            # print(row)
+                    self.table[key] /= tot
+            print(self.get_row(gram))
 
+    # returns KEYS for prev/next gram combos in a rowj
     def get_row(self, gram):
-        return self.table[gram]
-
-    def get_row_list(self, gram):
-        return [self.table[gram, gram2] for gram2 in self.grams]
+        row = {}
+        for key, value in self.table.iteritems():
+            if key[0] == gram:
+                row[key] = value
+        return row
 
     def get_col(self, gram):
         vals = [self.table[gram2, gram] for gram2 in self.grams]
