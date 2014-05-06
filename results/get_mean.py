87cf34b1704aadbe7aa625b295fc23684cf28301
scarafoni <dscarafo@u.rochester.edu>
2014-05-06 18:17:29 -0400

    test 3 of the nysc error

--- results/get_mean.py
+++ results/get_mean.py
@@ -3,10 +3,11 @@ import numpy
 import sys
 lines = [line.strip() for line in open(sys.argv[1])] 
 with open(sys.argv[2],'w') as fw:
+    fw.write('n,mean,std')
     for line in lines:
         line = line.split(',')
         if(len(line) < 2): continue
         line = [float(x) for x in line]
-        fw.write(str(numpy.mean(line))+' '+str(numpy.std(line))+'\n')
+        fw.write(str(len(line))+','+str(numpy.mean(line))+','+str(numpy.std(line))+'\n')
     fw.close()
         
