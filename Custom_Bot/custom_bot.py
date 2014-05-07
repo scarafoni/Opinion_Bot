3d6110a4f957adf2e5c68cce4d2d6aa94e01e0e3
scarafoni <dscarafo@u.rochester.edu>
2014-05-06 23:07:31 -0400

    green eggs and ham mc

--- Custom_Bot/custom_bot.py
+++ Custom_Bot/custom_bot.py
@@ -3,7 +3,7 @@ import sys
 import csv
 from nltk import ngrams
 import string
-from numpy import random, log2
+from numpy import random, log2, mean
 from transition_table import Transition_Table
 
 
@@ -160,17 +160,26 @@ def run_tests_mc(story, save_file, min_gram, max_gram):
         # number of test
         tests = []
         for i in range(min_gram, max_gram+1):
-            # print(i)
+            print('on gram '+str(i)+' of '+str(max_gram))
             test = [str(i)]
             bot = Custom_Bot(i, story)
             # print('grams', bot.grams)
-            tsize = len(bot.grams)
-            for i in range(100):
-                hint = random.choice(bot.grams)
-                # print('on hint', hint)
-                if not i % 10:
-                    print('on gram '+str(i)+' of '+str(tsize))
-                a = random.choice(bot.table.get_row(hint))
+            gram_keys = bot.grams.keys()
+            num_grams = len(gram_keys)
+            for j in range(100):
+                hint = random.randint(num_grams)
+                hint = gram_keys[hint]
+                row = bot.table.get_row(hint)
+                while len(row) == 0:
+                    hint = random.randint(num_grams)
+                    hint = gram_keys[hint]
+                    row = bot.table.get_row(hint)
+                ax = random.randint(len(row))
+                # print('ax',ax)
+                # print('hint',hint)
+                # print('key',row.keys())
+                a = row.keys()[ax]
+                # print('a',a)
                 a = a[-1]
                 # print('1', 'hint', 'answer', hint, a[-1])
                 # populate the list of tests
@@ -179,9 +188,8 @@ def run_tests_mc(story, save_file, min_gram, max_gram):
                 test += [err(hint, a)]
                 # print('test', test)
             tests.append(test)
-            print('test final', test)
-        # convert the tests to rows, print
-        # rows = zip(*tests)
+            # print('test final', test)
+            print('average',mean([float(x) for x in test[1:]]))
         for row in tests:
             result_csv.writerow(row)
         f.close()
