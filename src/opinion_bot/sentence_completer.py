030083f8cdc49d8b36752020b91ff948122fdb73
scarafoni <dan@scarafoni.com>
2014-08-02 17:17:40 -0400

    outline for sentence_completer class

---
+++ src/opinion_bot/sentence_completer.py
@@ -0,0 +1,14 @@
+"""
+given a sentence, this should be able to complete the next word
+"""
+
+class MyClass(object):
+    '''
+    given a complete sentence, this should be able to complete the next word
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
