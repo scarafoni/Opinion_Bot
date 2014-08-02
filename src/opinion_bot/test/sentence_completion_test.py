aa3ae67aba9e3829cc5c0e6fef25b15b78e2b260
scarafoni <dan@scarafoni.com>
2014-08-02 17:16:17 -0400

    unit test done

--- src/opinion_bot/test/sentence_completion_test.py
+++ src/opinion_bot/test/sentence_completion_test.py
@@ -1,13 +1,14 @@
 import unittest
-
-
 class sentence_completion_test(unittest.TestCase):
+    def test_complete_one(self):
+        sentence = "green eggs and";
+        correct_guess = "ham" 
+        sentence_completer  = Sentence_Completer("green eggs and ham",3)
+        guess = sentence_completer.guess(sentence)
+        self.asserEqual(guess,correct_guess,"didn't guess 'green eggs and ham'")
 
 
-    def testName(self):
-        pass
 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
